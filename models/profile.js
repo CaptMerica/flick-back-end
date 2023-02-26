6c3f31549123bb092353d641a7876e0842fd1f12
CaptMerica <rmerica@live.com>
2023-02-26 16:26:51 -0500

    update song and profile model

--- models/profile.js
+++ models/profile.js
@@ -1,15 +1,13 @@
 'use strict'
 const { Model } = require('sequelize')
+const song = require('./song')
 
 module.exports = (sequelize, DataTypes) => {
   class Profile extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       Profile.belongsTo(models.User, { foreignKey: 'userId' })
+      Profile.hasOne(models.Song, { foreignKey: 'songId' })
     }
   }
 
